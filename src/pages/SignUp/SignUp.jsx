8a0ad78cad28adc1f02720ca1f26127e5e54e0a1
Rana Almalki <ranaalmalki12@gmail.com>
2024-10-19 18:06:30 +0300

    start add form for the singup page

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div>
+
+      <div className='grid grid-col-2'>
+        <div className='border-2 p-4'>
+          <h2>Sign Up</h2>
+          <form>
+            <div className='mb-4'>
+              <label htmlFor='name'>Name</label>
+              <input type='text' id='name' name='name' required />
+            </div>
+            <div className='mb-4'>
+              <label htmlFor='email'>Email</label>
+              <input type='email' id='email' name='email' required />
+            </div>
+            <div className='mb-4'>
+              <label htmlFor='password'>Password</label>
+              <input type='password' id='password' name='password' required />
+            </div>
+            <div>
+              <button type='submit'>Sign Up</button>
+            </div>
+          </form>
+        </div>
+<div className='logo'>
+
+</div>
+        </div>
+
+      </div>
+
+
+
+    </div>
   )
 }
 
