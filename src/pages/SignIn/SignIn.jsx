53ccd15b87ffc80c24685b3af13c451cb6883a0d
Rana Almalki <ranaalmalki12@gmail.com>
2024-10-20 12:51:23 +0300

    add the styleing the page  sign in and input

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,8 +1,88 @@
-import React from 'react'
-
+import styles from "../../styles"
+import { logo } from "../../assets";
+import { BsFillPersonFill } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
 function SignIn() {
     return (
-        <div>SignIn</div>
+        <div className={`${styles.outerWrapper} bg-black lg:h-screen md:h-screen sm:h-screen max-sm:h-screen  flex justify-center items-center `}>
+            <div className={`${styles.wrapper}`}>
+                <div className="grid">
+                    <div className="logo">
+                    <img
+              src={logo}
+              alt="logo-falconeyeai"
+              className="object-contain object-center lg:w-full md:w-1/2 sm:w-1/2 max-sm:w-1/2"
+            />
+                    </div>
+                    <div className="lg:p-16">
+                    <h2 className={`${styles.heading2} text-white p-6 mb-5 md:text-center sm:text-center max-sm:text-center`}>Sign In</h2>
+            <form className="w-full max-w-md">
+              <div className="mb-4 flex items-center">
+                <div className="relative w-full">
+                  <div className="absolute left-2 top-1/2 transform -translate-y-1/2 text-[#1A1A1D] flex gap-2 items-center">
+                    <BsFillPersonFill className="h-8 w-8"/>
+                  </div>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Name"
+                    className="pl-12 border border-[#38383E] rounded-full focus:outline-none p-3 focus:ring focus:ring-blue-400 bg-[#38383E] bg-opacity-9 placeholder:text-[#1E1E1E] font-bold"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="mb-4 flex items-center  ">
+                <div className="relative w-full">
+                  <div className="absolute flex gap-2 items-center transform -translate-y-1/2 left-2 top-1/2 text-[#1A1A1D]">
+                    <MdEmail className=" h-8 w-8" />
+                  </div>
+
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    className="pl-12 border border-[#38383E] rounded-full focus:outline-none p-3 focus:ring focus:ring-blue-400 bg-[#38383E] bg-opacity-9 placeholder:text-[#1E1E1E] font-bold"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="mb-8 flex items-center ">
+                <div className="relative w-full">
+                  <div className="absolute flex gap-2 items-center transform -translate-y-1/2 left-2 top-1/2 text-[#1A1A1D]">
+                    <RiLockPasswordLine className="h-8 w-8"/>
+                  </div>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Password"
+                    className="pl-12 border border-[#38383E] rounded-full focus:outline-none p-3 focus:ring focus:ring-blue-400 bg-[#38383E] bg-opacity-9 placeholder:text-[#1E1E1E] font-bold"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <button type="submit" className="text-white bg-gradient-to-r from-[#333399] via-[#333399] to-[#FF00CC] rounded-full p-1 ">
+<div className="rounded-full bg-black p-3 ">
+                  Sign In
+                  </div>
+                </button>
+                <p className="mt-6 text-center text-white">
+                  Do not have an account? <a href="#" className="text-transparent bg-clip-text bg-gradient-to-r from-[#333399] via-[#333399] to-[#FF00CC]
+                  font-bold">Sign Up</a>
+                </p>
+              </div>
+            </form>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
