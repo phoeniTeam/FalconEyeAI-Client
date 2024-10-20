5cea4da96dc07e1f6f2dac0cf593f5d617edad5c
Rana Almalki <ranaalmalki12@gmail.com>
2024-10-20 13:26:51 +0300

    make the signin responsive

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -7,9 +7,9 @@ function SignIn() {
     return (
         <div className={`${styles.outerWrapper} bg-black lg:h-screen md:h-screen sm:h-screen max-sm:h-screen  flex justify-center items-center `}>
             <div className={`${styles.wrapper}`}>
-                <div className="grid">
-                    <div className="logo">
-                    <img
+            <div className="grid lg:grid-cols-2 md:grid-cols-1   sm:grid-cols-1 max-sm:grid-cols-1  md:h-screen sm:h-screen max-sm:h-screen lg:items-center lg:justify-items-center  md:justify-items-center  ">
+            <div className="logo flex md:items-end justify-center sm:justify-center sm:items-end max-sm:justify-center max-sm:items-end">
+            <img
               src={logo}
               alt="logo-falconeyeai"
               className="object-contain object-center lg:w-full md:w-1/2 sm:w-1/2 max-sm:w-1/2"
@@ -18,21 +18,6 @@ function SignIn() {
                     <div className="lg:p-16">
                     <h2 className={`${styles.heading2} text-white p-6 mb-5 md:text-center sm:text-center max-sm:text-center`}>Sign In</h2>
             <form className="w-full max-w-md">
-              <div className="mb-4 flex items-center">
-                <div className="relative w-full">
-                  <div className="absolute left-2 top-1/2 transform -translate-y-1/2 text-[#1A1A1D] flex gap-2 items-center">
-                    <BsFillPersonFill className="h-8 w-8"/>
-                  </div>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder="Name"
-                    className="pl-12 border border-[#38383E] rounded-full focus:outline-none p-3 focus:ring focus:ring-blue-400 bg-[#38383E] bg-opacity-9 placeholder:text-[#1E1E1E] font-bold"
-                    required
-                  />
-                </div>
-              </div>
               <div className="mb-4 flex items-center  ">
                 <div className="relative w-full">
                   <div className="absolute flex gap-2 items-center transform -translate-y-1/2 left-2 top-1/2 text-[#1A1A1D]">
