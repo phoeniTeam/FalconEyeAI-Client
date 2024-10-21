9cc30f5c953978b990e02cbeee8f1d8132c3993c
Rana Almalki <ranaalmalki12@gmail.com>
2024-10-21 20:08:30 +0300

    add the title and credition in the restoreimage

--- src/pages/Transformation/ImageRestore.jsx
+++ src/pages/Transformation/ImageRestore.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import styles from '../../styles';
+import CreditIcon from "../../assets/icons/creditIcon"; 
 
 function ImageRestore() {
   return (
-    <div>ImageRestore</div>
+    <div>
+            <div className="py-0 px-3 w-full flex flex-col justify-between gap-8 ">
+            <div className={`grid grid-cols-1 gap-10 p-8 w-full `}>
+            <div className="flex justify-between ">
+                  
+                  <div className="flex flex-col items-start gap-4">
+                  <h1 className={`${styles.heading3} text-white `}>
+                      Image Restore
+                  </h1>
+                  <div className={`${styles.paragraph2} text-white `}>
+                    Refine images by removing noise and imperfections
+
+                  </div>
+                  <div className="flex items-center justify-start gap-4">
+                         
+                             <CreditIcon />
+                      <div className={`${styles.paragraph2} text-white `}>
+                          12
+                      </div>
+                  </div>
+              </div>
+                  <div className="flex  gap-2">
+                  <CreditIcon />
+
+                      <div className={`${styles.heading4} text-white `}>
+                          236
+                      </div>
+                      
+                  </div>
+              </div>
+</div>
+</div>
+    </div>
   )
 }
 
