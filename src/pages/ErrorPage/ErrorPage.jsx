381a085b70e50a4085cbed27c75cd61a4d482889
comsaziz <comsaziz2@gmail.com>
2024-10-22 22:57:18 +0300

    Edit Home and Profile and Errorpage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "../../styles"; 
+import styles from "../../styles";
 
 export default function ErrorPage() {
   const navigate = useNavigate();
@@ -10,8 +10,10 @@ export default function ErrorPage() {
   };
 
   return (
-    <main className={`w-screen h-screen flex flex-col justify-center items-center ${styles.primaryBackground}`}>
-      <div className={`${styles.wrapper} text-center`}>
+    <main className={`w-screen h-screen flex flex-col justify-center items-center relative ${styles.primaryBackground}`}>
+      {/* Background Layer */}
+      <div className="opacity-90 absolute -translate-x-1/2 -translate-y-1/2 -top-10 left-1/2 w-full h-[1500px] rounded-full filter blur-[100px] bg-gradient-to-r from-blue-500 to-purple-500"></div>
+      <div className={`${styles.wrapper} text-center relative z-10`}>
         <h1 className={`${styles.heroHeading} text-white animate-bounce`}>
           404
         </h1>
