98ffe3428265c2ac9fa80ad026563b50bce5ff09
comsaziz <comsaziz2@gmail.com>
2024-10-23 18:48:37 +0300

    Add New styles and Component

--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,4 @@
-const mainTransition = "transition-all duration-500 ease-in-out"
+const mainTransition = "transition-all duration-500 ease-in-out";
 
 const styles = {
     outerWrapper: "py-10 md:py-20 h-auto",
@@ -6,7 +6,7 @@ const styles = {
     wrapper: "max-w-[1200px] px-4 mx-auto",    
     innerWrapper:"py-8 px-9",
     outerWrapperAppPages:"max-w-[1300px] mx-auto",
- 
+
     heroHeading: "text-5xl sm:text-6xl md:text-7xl font-bold",
     heading1: "text-4xl sm:text-5xl md:text-6xl font-bold",
     heading2: "text-3xl sm:text-4xl md:text-5xl font-semibold",
@@ -21,12 +21,14 @@ const styles = {
     primaryBackground: "bg-primary-gradient-color",
     primaryButton: "px-6 py-1 rounded-full bg-primary-gradient-color hover:bg-primary-gradient-color-invert text-center h-fit",
 
-
+    // New button styles
+    newGradientButton: "text-white bg-gradient-to-r from-[#333399] via-[#333399] to-[#FF00CC] rounded-full p-0.5",
+    newInnerButton: `rounded-full bg-[#211f33] py-0.5 px-12 ${mainTransition} hover:bg-gradient-to-r from-[#333399] via-[#333399] to-[#FF00CC]`,
 
     primaryText: `w-fit bg-gradient-to-r from-primary via-primary to-secondary inline-block text-transparent bg-clip-text ${mainTransition}`,
     primaryTextOnHover: `hover:underline hover:bg-gradient-to-r from-primary via-primary to-secondary inline-block hover:text-transparent bg-clip-text ${mainTransition}`,
 
     transition500: `${mainTransition}`
-}
+};
 
 export default styles;
