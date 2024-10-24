3847dcdaf38e883714be4f323e587764c99ed15f
comsaziz <comsaziz2@gmail.com>
2024-10-24 10:48:18 +0300

    Edit the CustomIcon

--- src/assets/icons/CustomIcon.jsx
+++ src/assets/icons/CustomIcon.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-function CustomIcon({ size = 20 }) {
+function CustomIcon( { width = 20, height = 20 }) {
   return (
     <svg
-      stroke="currentColor"
-      fill="currentColor"
-      strokeWidth="0"
-      viewBox="0 0 32 32"
-      height={size}
-      width={size}
+      width={width}
+      height={height}
+      viewBox="0 0 20 20"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-       <path d="M16 4C9.383 4 4 9.383 4 16s5.383 12 12 12 12-5.383 12-12S22.617 4 16 4zm0 2c5.535 0 10 4.465 10 10s-4.465 10-10 10S6 21.535 6 16 10.465 6 16 6zm0 7a3 3 0 1 0 .002 6.002A3 3 0 0 0 16 13z" 
-       fill="url(#paint0_linear_159_7)" />
-  <defs>
+      <path
+        d="M18.3356 9.996C18.3356 14.5984 14.6047 18.3293 10.0022 18.3293C5.3999 18.3293 1.66895 14.5984 1.66895 9.996C1.66895 5.39366 5.3999 1.66266 10.0022 1.66266C14.6047 1.66266 18.3356 5.39366 18.3356 9.996ZM6.46674 9.996L10.0022 13.5315L13.5378 9.996L10.0022 6.4605L6.46674 9.996Z"
+        fill="url(#paint0_linear_332_2)"
+      />
+      <defs>
         <linearGradient
-          id="paint0_linear_159_7"
-          x1="3.33789"
-          y1="19.9918"
-          x2="36.6711"
-          y2="19.9918"
+          id="paint0_linear_332_2"
+          x1="1.66895"
+          y1="9.996"
+          x2="18.3356"
+          y2="9.996"
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor="#333399" />
@@ -27,10 +27,6 @@ function CustomIcon({ size = 20 }) {
           <stop offset="1" stopColor="#FF00CC" />
         </linearGradient>
       </defs>
-
-
-
-     
     </svg>
   );
 }
