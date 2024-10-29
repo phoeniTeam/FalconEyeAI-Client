358d83874988cd7d6f481f15d546262044ad29f7
comsaziz <comsaziz2@gmail.com>
2024-10-29 21:27:05 +0300

    Edit Credit and add 3 icons

---
+++ src/assets/icons/TbJewishStarFilled.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const TbJewishStarFilled = () => (
+    <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+      <linearGradient
+        id="paint0_linear_159_7"
+        x1="3.33789"
+        y1="19.9918"
+        x2="36.6711"
+        y2="19.9918"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#4D91FF" />
+        <stop offset="0.5" stopColor="#620A9C" />
+        <stop offset="1" stopColor="#FF00CC" />
+      </linearGradient>
+    </defs>
+    <path 
+      d="M8.433 6H3l-.114.006a1 1 0 0 0-.743 1.508L4.833 12l-2.69 4.486-.054.1A1 1 0 0 0 3 18h5.434l2.709 4.514.074.108a1 1 0 0 0 1.64-.108L15.565 18H21l.114-.006a1 1 0 0 0 .743-1.508L19.166 12l2.691-4.486.054-.1A1 1 0 0 0 21 6h-5.434l-2.709-4.514a1 1 0 0 0-1.714 0L8.433 6z" 
+      fill="url(#paint0_linear_159_7)" 
+      stroke="none"
+    />
+  </svg>
+
+  
+);
+
+export default TbJewishStarFilled;
