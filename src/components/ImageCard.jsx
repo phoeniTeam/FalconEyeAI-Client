28a809013adcd4faed9193ff059570db322ac119
mohammed-dhabaab <mohammed.dhabaab@gmail.com>
2024-10-29 14:19:20 +0300

    Improved the imageCard

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const ImageCard = ({ image, description, userImage, userName }) => {
   return (
-    <div className="relative mb-4 break-inside-avoid cursor-pointer group">
+    <div className="relative mb-4 break-inside-avoid cursor-pointer group overflow-hidden rounded-xl">
       <img
-        className="transition duration-500 rounded-xl opacity-80 group-hover:opacity-100"
+        className="transition duration-500 rounded-xl opacity-80 group-hover:opacity-100 group-hover:scale-[1.02]"
         src={image}
         alt="Creation"
       />
       <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-between  opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 
         {userImage && userName && (
-          <div className="flex items-center gap-2 p-2 rounded-lg w-full">
+          <div className="flex items-center gap-2 p-2 w-full">
             <img
               src={userImage}
               alt="User"
@@ -23,7 +23,7 @@ const ImageCard = ({ image, description, userImage, userName }) => {
 
 
         <p className="text-darkWhite text-xs px-5  bottom-3 p-2">
-          {description}
+          {description.charAt(0).toUpperCase() + description.slice(1)}
         </p>
       </div>
     </div>
